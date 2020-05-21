2e26f1f4453f122ab88ca77a13950b65c9bd658c
d3x <prasky.it@gmail.com>
2020-05-21 15:15:49 +0200

    200521-awesomefonts-add

--- admincp/index.php
+++ admincp/index.php
@@ -1,3 +1,15 @@
+<!doctype html>
+      <html>
+        <head>
+            <link href="css/all.css" rel="stylesheet"> <!--load all styles -->
+        </head>
+
+        <body>
+          <i class="fas fa-thumbs-up fa-5x"></i>
+        </body>
+      </html>
+
+
 <?php
 
 //Test
