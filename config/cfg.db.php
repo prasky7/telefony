9e74d9403b56016d6842ed1cdd9ed0419572a130
d3x <prasky.it@gmail.com>
2020-05-21 14:06:04 +0200

    master

---
+++ config/cfg.db.php
@@ -0,0 +1,8 @@
+<?php
+$dbserver     = "localhost";
+$dbname       = "telefony";
+$dbuser       = "telefony";
+$dbpass       = "EH4Ykwg3HkvUMSpw";
+$table_prefix = "ab_";
+$keep_history = true;
+?>
---
+++ config/cfg.user.php
@@ -0,0 +1,50 @@
+<?php
+  //
+  // == List of Login/Pass-Users ==
+  //
+  // -- Setup an "admin" user, with password "secret" --
+  $userlist['admin_telefony']['pass'] = "secret636";
+  $userlist['admin']['role'] = "root"; // used to call "/diag.php"
+
+  //*  
+  // -- Setup readonly-user for regression tests (yourdomain.com/addressbook/test/
+  $userlist['simpletest']['pass']   = "simple";
+  $userlist['simpletest']['role']   = "readonly";  
+  $userlist['simpletest']['domain'] = 9999;
+  //*/
+
+  /*  
+  // Setup a "readonly" user
+  $userlist['view']['pass']  = "apassword";
+  $userlist['view']['role']  = "readonly";  
+
+  // Setup a user accessing only one group
+  $userlist['mygroup']['pass']  = "apassword";
+  $userlist['mygroup']['group'] = "My group";
+
+  // Setup a user for the second domain (0 = default)
+  $userlist['adm2']['pass']   = "adm2";
+  $userlist['adm2']['domain'] = 1;
+  //*/
+
+  //
+  // == User table in database ==
+  // - Excludes the table_prefix!
+  $usertable = "users";
+
+  // == List of IP-Users ==  
+  $iplist['*.*.*.*']['role']  = "readonly";
+  //$iplist['192.168.221.11']['role']  = "admin";
+
+
+  //
+  // == Look & Feel of the domains
+  //
+  $skin_color = "green"; // global skin color, e.g. on login
+
+  // blue, brown, green, grey, pink, purple, red, turquoise, yellow
+  $domain[0]['skin'] = "green";
+  $domain[1]['skin'] = "green";
+  $domain[2]['skin'] = "green";
+
+?>
