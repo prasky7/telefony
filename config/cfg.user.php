7c206346fa4e6ac8abe0f8bf20b0528ceb4a8f92
d3x <prasky.it@gmail.com>
2020-05-21 14:20:14 +0200

    200521-heslo

--- config/cfg.user.php
+++ config/cfg.user.php
@@ -3,20 +3,20 @@
   // == List of Login/Pass-Users ==
   //
   // -- Setup an "admin" user, with password "secret" --
-  $userlist['admin_telefony']['pass'] = "secret636";
+  $userlist['admin']['pass'] = "admin";
   $userlist['admin']['role'] = "root"; // used to call "/diag.php"
 
-  //*  
+  //*
   // -- Setup readonly-user for regression tests (yourdomain.com/addressbook/test/
   $userlist['simpletest']['pass']   = "simple";
-  $userlist['simpletest']['role']   = "readonly";  
+  $userlist['simpletest']['role']   = "readonly";
   $userlist['simpletest']['domain'] = 9999;
   //*/
 
-  /*  
+  /*
   // Setup a "readonly" user
   $userlist['view']['pass']  = "apassword";
-  $userlist['view']['role']  = "readonly";  
+  $userlist['view']['role']  = "readonly";
 
   // Setup a user accessing only one group
   $userlist['mygroup']['pass']  = "apassword";
@@ -32,7 +32,7 @@
   // - Excludes the table_prefix!
   $usertable = "users";
 
-  // == List of IP-Users ==  
+  // == List of IP-Users ==
   $iplist['*.*.*.*']['role']  = "readonly";
   //$iplist['192.168.221.11']['role']  = "admin";
 
