18fda7e39f8f2015322a136bf28b579f87b3f7c7
d3x <prasky.it@gmail.com>
2020-06-16 11:53:08 +0200

    200616-excel-002

--- excel/index.php
+++ excel/index.php
@@ -6,7 +6,10 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+// $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+// $spreadsheet = $reader->load("upload/test.xlsx");
 $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+$reader->setLoadSheetsOnly(["aktuální"]);
 $spreadsheet = $reader->load("upload/test.xlsx");
 $writer = new \PhpOffice\PhpSpreadsheet\Writer\Pdf\Mpdf($spreadsheet);
 $writer->setSheetIndex(0);
